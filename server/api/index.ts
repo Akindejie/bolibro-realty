bf456c6601f0aaaa45c506968db6f539d4bb2530
Akindejie <fuddisnalk@gmail.com>
2025-04-14 16:58:35 -0400

    deploy commit

--- server/api/index.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-import app from '../src/vercel';
-
-export default async function handler(req: Request, res: Response) {
-  // Let the Express app handle the request
-  return app(req, res);
-}
